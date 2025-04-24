5c151316b4bf29630f8fda9ff52b908d24660575
siyancsijo <siyansijoc@gmail.com>
2025-04-24 23:41:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,9 +4,11 @@ const ASSETS_TO_CACHE = [
   '/index.html',
   '/styles.css',
   '/app.js',
+  '/manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://unpkg.com/@turf/turf@6/turf.min.js'
 ];
 
 self.addEventListener('install', (event) => {
